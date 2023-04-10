62c3b7f8e7a418748fa39d5fd81c61e682c76210
Fernando Angulo <57889477+luisfernandoAngulo28@users.noreply.github.com>
2023-04-10 00:23:11 -0400

    Ayudantia
    Ayudantia

---
+++ Natural/CApp/CApp/clsNatu.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CApp
+{
+  public class clsNatu
+    {
+        // Atributo
+        private int _n;
+
+        // Propiedades
+        public int n
+        {
+            get { return _n; }
+            set { _n = value; }
+        }
+
+        // Constructores
+
+        public clsNatu() { n = 0; }
+
+        public clsNatu(int x)
+        {
+            if (x >= 0)
+            {
+                this.n = x;
+            }
+            else this.n = 0;
+        }
+
+        public clsNatu(clsNatu t)
+        {
+            if (t.n >= 0)
+            {
+                this.n = t.n;
+            }
+            else this.n = 0;
+        }
+
+        // Funciones
+        public int Cero()
+        {
+            return 0;
+        }
+
+        public bool EsCero(clsNatu x)
+        {
+            return (x.n == 0);
+        }
+
+        public clsNatu Sucesor(clsNatu x)
+        {
+            x.n = x.n + 1;
+            return (x);
+        }
+
+        public clsNatu Predecesor(clsNatu x)
+        {
+            if (EsCero(x) == false)
+            {
+                x.n = x.n - 1;
+            }
+            return x;
+        }
+
+
+        public bool Igual(clsNatu x, clsNatu y)
+        {
+            if (EsCero(x) == true & EsCero(y) == false)
+                return false;
+
+            if (EsCero(x) == false & EsCero(y) == true)
+                return false;
+
+            if (x == y)
+                return true;
+            return false;
+        }
+
+        public clsNatu Suma(clsNatu a, clsNatu b)
+        {
+            if (EsCero(a) == true & EsCero(b) == false)
+                return b;
+            if (EsCero(a) == false & EsCero(b) == true)
+                return a;
+            clsNatu s = new clsNatu();
+            s.n = a.n + b.n;
+            return s;
+        }
+
+
+    }
+}
---
+++ Natural/WApp/WApp/FNatural.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CApp;
+
+namespace WApp
+{
+    public partial class FNatural : Form
+    {
+        public FNatural()
+        {
+            InitializeComponent();
+        }
+
+        clsNatu Natu = new clsNatu();
+        clsNatu Nume = new clsNatu();
+        clsNatu Nume1 = new clsNatu();
+        clsNatu Nume2 = new clsNatu();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numero = int.Parse(textBox1.Text);
+            //int numero = Convert.ToInt32(textBox1.Text);
+
+            Nume = new clsNatu(numero);
+            //label1.Text = (Natu.Sucesor(Nume).n).ToString();
+            label2.Text = Convert.ToString(Natu.Sucesor(Nume).n);
+
+        }
+    }
+}
